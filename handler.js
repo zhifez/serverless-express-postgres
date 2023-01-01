8549685db607260892bc40437d3a42f5f9220266
zhifez <zhifez.lee@gmail.com>
2023-01-01 22:26:29 +0800

    Still working on serverless config, at this point I'm convinced the handler.js isn't the problem but something to do with security groups configs

--- handler.js
+++ handler.js
@@ -3,25 +3,37 @@ const express = require('express');
 const app = express();
 const { Client } = require('pg');
 const client = new Client({
-  user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
+  port: 5432,
 });
-client.connect();
 
-app.get('/', async (req, res, next) => {
-  await client.query(`
-    CREATE TABLE IF NOT EXISTS users (
-      ID SERIAL PRIMARY KEY,
-      name VARCHAR(30),
-      email VARCHAR(30)
-    );
-  `);
+app.get('/', async (_, res) => {
+  try {
+    console.log('connect to db');
 
-  return res.status(200).json({
-    message: 'Table created!',
-  });
+    await client.connect();
+
+    console.log('create table if not exists');
+
+    const results = await client.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        ID SERIAL PRIMARY KEY,
+        name VARCHAR(30),
+        email VARCHAR(30)
+      );
+    `);
+
+    await client.end();
+
+    return res.status(200).json({
+      message: 'Table created!',
+    });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 });
 
 app.get('/hello', (req, res, next) => {
