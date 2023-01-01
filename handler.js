dec401682680f528dafa827511cbd8ff0caa9aaa
zhifez <zhifez.lee@gmail.com>
2023-01-01 23:37:10 +0800

    Removed constant client.connect and client.end, and activate it once at the top of handler.js

--- handler.js
+++ handler.js
@@ -10,6 +10,7 @@ const client = new Client({
   password: process.env.POSTGRES_PASSWORD,
   port: 5432,
 });
+client.connect();
 
 app.use(bodyParser.json());
 app.use(
@@ -26,10 +27,6 @@ app.get('/', (req, res, next) => {
 
 app.get('/users', async (_, res) => {
   try {
-    console.log('connect to db');
-
-    await client.connect();
-
     console.log('create table if not exists');
 
     await client.query(`
@@ -46,15 +43,12 @@ app.get('/users', async (_, res) => {
       SELECT * from users
     `);
 
-    await client.end();
-
     return res.status(200).json({
       message: 'Users retrieved successfully',
       data: results.rows,
     });
   } catch (error) {
     console.log(error);
-    await client.end();
     return res.status(500).json({
       error: error.message,
     });
@@ -71,10 +65,6 @@ app.post('/users', async (req, res) => {
       throw new Error(`"name" and "email" fields are required`);
     }
 
-    console.log('connect to db');
-
-    await client.connect();
-
     console.log('create user');
 
     await client.query(`
@@ -82,14 +72,11 @@ app.post('/users', async (req, res) => {
       VALUES ('${name}', '${email}')
     `);
 
-    await client.end();
-
     return res.status(200).json({
       message: 'User created',
     });
   } catch (error) {
     console.log(error);
-    await client.end();
     return res.status(500).json({
       error: error.message,
     });
